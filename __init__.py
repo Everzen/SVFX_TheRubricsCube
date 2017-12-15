0487fe6164e370fcda6b63b8168a8a9485ee182f
Everzen <3dframework@gmail.com>
2017-12-15 18:27:11 +0000

    Starting main Init Tab Widget

--- __init__.py
+++ __init__.py
@@ -32,10 +32,167 @@ for student in root[0][0]:
 	studentDetail = {'id':student.get('BoltonID'), 'forename':student.get('forename1'), 'surname':student.get('surname'), 'email':student.get('contactEMail'), 'occurence':student.get('Occurrence'), 'course':student.get('courseName'), 'attendence':'A', 'lateness':0}
 	studentList.append(studentDetail)
 
+loanList = [studentList[0], studentList[1]]
+
+
+class SVFX_AssetTrackerUI(QDialog):
+    def __init__(self, parent=None):
+        super(SVFX_AssetTrackerUI, self).__init__(parent)
+
+        tabLayout = QVBoxLayout()
+        self.userListTV = widgets.userTV(studentList)
+        self.setMinimumSize(800,600)
+        self.tabWidget = QTabWidget()
+        self.tabWidget.addTab(self.userListTV, "Users")
+        self.tabWidget.addTab(QWidget(), "Relative")
+        self.tabWidget.addTab(QWidget(), "Questions")
+        self.tabWidget.addTab(QWidget(), "Quest Specific")
+
+        tabLayout.addWidget(self.tabWidget)
+
+
+        userMessageLayout = QVBoxLayout()
+        userColumnWidth = 150
+        userTabWidth = 500
+        
+        #Setup regarding List - This is going to dictate who the message is about
+        regardLabel = QLabel("REGARDING:")
+        regardingListLW =  QListWidget()
+        regardingListLW.setMaximumWidth(userColumnWidth)
+
+        # regardingListLW.insertItems(0, regardingList)
+
+        #set up Message From List
+        voiceListLW =  QListWidget()
+        voiceListLW.setMaximumWidth(userColumnWidth)
+        voices =[]
+
+        #Grab all the voices
+        voiceLabel = QLabel("VOICE ORIGIN:")
+        # for text in voiceList:
+        #     voices.append(text["name"])
+
+        voiceListLW.insertItems(0, voices)  
+        voiceListLW.setCurrentRow(0)
+        voiceListLW.setMaximumHeight(19*len(voices))
+
+        #Setup player List
+        playerLabel = QLabel("SEND TO PLAYER:")
+        playerListLW =  QListWidget()
+        playerListLW.setMaximumWidth(userColumnWidth)
+        players = []
+
+        #Grab all the Players
+        # for text in playerList:
+        #     players.append(text["character"] + " (" + text['name'] + ")")
+
+        playerListLW.insertItems(0, players)
+        playerListLW.setMaximumHeight(18*len(players))
+        playerListLW.setSelectionMode(QAbstractItemView.ExtendedSelection)
+
+        def findActiveStatement():
+        	"""This function returns the listwidget and the selected row in a dictionary"""
+        	#Find the list Widget in the Current Tab
+        	activeList = self.tabWidget.currentWidget().layout().itemAt(0).widget()
+        	# print activeList
+        	# print type(activeList)
+        	if len(activeList.selectedIndexes()) != 0:
+        		# print activeList.selectedIndexes()[0].row()
+        		return {'activeListWidget':activeList, "activeStatement" : activeList.selectedIndexes()[0].row()}
+        	else:
+        		return {'activeListWidget':None, "activeStatement" : None}
+
+        def findActiveRegard():
+        	if len(regardingListLW.selectedIndexes()) != 0:
+        		# print "Active Regard: " + regardingListLW.item(regardingListLW.selectedIndexes()[0].row()).text()
+        		return regardingListLW.item(regardingListLW.selectedIndexes()[0].row()).text()
+        	else:
+        		return None
+
+        def findActiveVoice():
+        	# print "Active Voice: " + str(voiceListLW.selectedIndexes()[0].row())
+        	return voiceListLW.selectedIndexes()[0].row()
+
+        def findActivePlayers():
+        	activePlayerList = []
+        	for index in playerListLW.selectedIndexes():
+        		activePlayerList.append(index.row())
+        	# print activePlayerList
+        	return activePlayerList
+
+        def processMessage():
+        	activeStatementDict = findActiveStatement()
+        	if activeStatementDict["activeListWidget"]:
+        		statement = activeStatementDict["activeListWidget"].item(activeStatementDict["activeStatement"]).text()
+        		#Now check if the statement splits - if it does then it requires a regard 
+        		splitStatement = statement.split("####")
+        		if len(splitStatement) > 1:
+        			#The statement has split so collect the regard
+        			activeRegard = findActiveRegard()
+        			if activeRegard:
+        				#Awkard check code to see where we insert the name. If both sections of the split string has characters we insert in the middle. If not we insert on the empty string
+        				insertTarget = 2 #this represents the name having to be inserted into the middle
+        				for i, splitPhrase in enumerate(splitStatement):
+        					if len(splitPhrase) == 0: insertTarget = i
+        				#Now we hardcode build the string - messy!
+        				if insertTarget == 0:
+        					statement = "*" + activeRegard + "*" + splitStatement[1]
+        				elif insertTarget == 1:
+        					statement = splitStatement[0] + "*" + activeRegard + "*"
+        				elif insertTarget == 2:
+        					statement = splitStatement[0] + "*" + activeRegard + "*" + splitStatement[1]
+        			else: return None
+        		return statement
+        	else: 
+        		return None
+
+        def sendMessage():
+        	finalVoice = findActiveVoice()
+        	# print "Voice: " + str(finalVoice)
+        	finalPlayers = findActivePlayers()
+        	# print "Players: " + str(findActivePlayers)
+        	finalStatement = processMessage()
+        	# print "Statement: " + str(findActiveStatement)
+        	# print "My final Statement is: " + str(finalStatement)
+        	if (len(finalPlayers) != 0) and finalStatement: #If all if these conditions are met then we should be able to send the message
+        		voicename = voiceList[finalVoice]["name"]
+        		asUser = int(voiceList[finalVoice]["asUser"])
+        		print "Message: " + finalStatement
+        		for playerNum in finalPlayers:
+        			playerID = playerList[playerNum]["slackID"]
+        			slack_client.api_call("chat.postMessage", channel=playerID, text=finalStatement, as_user=asUser, username=voicename )    #Using text to add an emoji
+        			print "SENDING TO: " + playerList[playerNum]["character"]
+        	else:
+        		print "Message failed to SEND"
+
+
+        sendButton = QPushButton("SEND SLACK MESSAGE")
+        sendButton.setMinimumHeight(50)
+        sendButton.setMaximumWidth(userColumnWidth)
+        sendButton.clicked.connect(sendMessage)
+
+        userMessageLayout.addWidget(regardLabel)
+        userMessageLayout.addWidget(regardingListLW)
+        userMessageLayout.addWidget(voiceLabel)
+        userMessageLayout.addWidget(voiceListLW)
+        userMessageLayout.addWidget(playerLabel)
+        userMessageLayout.addWidget(playerListLW)
+        userMessageLayout.addWidget(sendButton)
+
+        mainLayout = QHBoxLayout()
+        mainLayout.addLayout(tabLayout)
+        mainLayout.addLayout(userMessageLayout)
+
+        # mainLayout.addWidget(tabWidget)
+        # mainLayout.addWidget(buttonBox)
+        self.setLayout(mainLayout)
+
+        self.setWindowTitle("Dungeon Master - Fast Messenger")
+
 
 
 #######################################Load Program#######################################################
-reg = widgets.userTV(studentList)
+reg = SVFX_AssetTrackerUI()
 reg.show()
 # view.show()
 sys.exit(app.exec_())
--- widgets.py
+++ widgets.py
@@ -10,6 +10,83 @@ from operator import itemgetter
 class userTV(QTreeWidget):
 		def __init__(self, userList, parent = None):
 			QTreeWidget.__init__(self, parent)
+			self.setMinimumSize(800,600)
+			self.setSelectionBehavior(QAbstractItemView.SelectRows)
+			self.setSelectionMode(QAbstractItemView.MultiSelection)
+			# self.model = QStandardItemModel()
+			self.setColumnCount(5)
+			self.totalHeaders = ['ID','Forename', 'Surname', 'Email', 'Course']
+			self.activeHeaders = []
+			self.setHeaderLabels(self.totalHeaders)
+			self.headerMapping = {'ID':'id', 'Forename':'forename', 'Surname':'surname', 'Email':'email', 'Course':'course'}
+			self.showHeaders = {'ID':True, 'Forename':True, 'Surname':True, 'Email':True, 'Course':True}
+			# self.setModel(self.model)
+			self.setUniformRowHeights(True)
+			self.userList = userList
+			# self.reSortList('surname')
+			# self.reSortList('forename')
+			self.setContextMenuPolicy(Qt.CustomContextMenu) #context menu for user data
+			self.customContextMenuRequested.connect(self.userMenu)
+			self.header().setContextMenuPolicy(Qt.CustomContextMenu) #context menu for headers
+			self.header().customContextMenuRequested.connect(self.headerMenu)
+			self.populateTreeList()
+
+		def collectActiveHeaders(self): 
+			"""Collect all the visible columns together"""
+			headers = []
+			for header in self.totalHeaders:
+				if self.showHeaders[header]: headers.append(header)
+			return headers
+
+		def populateTreeList(self):
+			self.clear()
+			self.activeHeaders = self.collectActiveHeaders()
+			for user in self.userList:
+				userData = []
+				for header in self.activeHeaders: 
+					userData.append(user[self.headerMapping[header]])
+				userTWItem = QTreeWidgetItem(self,userData)
+				self.addTopLevelItem(userTWItem)
+
+		def reSortList(self, userKey):
+			self.clear()
+			self.userList = sorted(self.userList, key=itemgetter(userKey))
+			self.populateTreeList()
+
+		def userMenu(self, position):
+			menu = QMenu()
+			menu.addAction(self.tr("Copy Email"))
+			menu.addAction(self.tr("Sort"))
+			menu.exec_(self.viewport().mapToGlobal(position))
+
+		def headerMenu(self, position):
+			menu = QMenu()
+			col = self.columnAt(position.x())
+			print (col)
+			headerText = self.headerItem().text(col)
+			# sort = menu.addAction(self.tr("Sort by " + headerText))
+			sortText =  ("Sort by " +  str(headerText))
+			sort = menu.addAction(self.tr(sortText))
+			hide = menu.addAction(self.tr("Hide"))
+			menu.addSeparator()
+			badger = menu.addAction(self.tr("badger"))  # Add in the show column list here
+			action = menu.exec_(self.viewport().mapToGlobal(position))
+			if action == sort: 
+				self.reSortList(self.headerMapping[headerText])
+				print ("map order" + str(self.headerMapping[headerText]))
+			if action == hide: #Turn off the showheader so populateTreeList regenerates the table without that header
+				self.showHeaders[headerText] = False
+				self.activeHeaders = self.collectActiveHeaders()
+				self.setColumnCount(len(self.activeHeaders))
+				self.setHeaderLabels(self.activeHeaders)
+				self.populateTreeList()
+
+
+
+class loanTV(QTreeWidget):
+		def __init__(self, userList, parent = None):
+			QTreeWidget.__init__(self, parent)
+			self.setMinimumSize(800,600)
 			self.setSelectionBehavior(QAbstractItemView.SelectRows)
 			self.setSelectionMode(QAbstractItemView.MultiSelection)
 			# self.model = QStandardItemModel()
